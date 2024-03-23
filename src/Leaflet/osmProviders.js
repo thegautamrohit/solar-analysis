13880ec55fa9a39f6a80b7e646ff22ba26bffb04
thegautamrohit <rgautam1997@gmail.com>
2024-03-23 22:35:57 +0530

    Basic map layout with select draw

---
+++ src/Leaflet/osmProviders.js
@@ -0,0 +1,5 @@
+export default {
+  maptiler: {
+    url: "https://api.maptiler.com/maps/basic-v2/256/tiles.json?key=vyKzluYxNETrn0wFksae",
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import { Inter } from "next/font/google";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../components/Map/MapComponent"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main>
-      <h1>SOLAR ANALYSIS</h1>
+      <Map />
     </main>
   );
 }
